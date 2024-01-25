4434d8e4102224dc54859131a581211fa932467e
Zongamin <dokbasti77@gmail.com>
2024-01-25 17:06:12 +0100

    Goal reached in C++

--- Konto/Prg.cpp
+++ Konto/Prg.cpp
@@ -2,14 +2,38 @@
 #include <iostream>
 #include <string>
 
+void output(Konto konto[])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << "Konto " << (i+1) << ": " << konto[i].getKontostand() << "\n==========" << std::endl;
+    }
+    
+}
+
+void line()
+{
+    std::cout << "\n==========" << std::endl;
+    return;
+}
+
 int main() 
 {
     Konto konto[3];
+    std::cout << "Initalisierter Kontostand: " << std::endl;
+    line();
+    output(konto);
+        
+    int neuerKontostand = konto[0].getKontostand() * 2;
+    konto[0].setKontostand(neuerKontostand);
+    neuerKontostand = konto[1].getKontostand() * 3;
+    konto[1].setKontostand(neuerKontostand);
+    neuerKontostand = konto[2].getKontostand() * 10;
+    konto[2].setKontostand(neuerKontostand);
 
-    for (int i = 0; i < 3; i++)
-    {
-        std::cout << "Konto " << (i+1) << ": " << konto[i].getKontostand() << "\n---------\n";
-    }
+    std::cout << "Neuer Kontostand: " << std::endl;
+    line();
+    output(konto);
 
     return 0;
 }
